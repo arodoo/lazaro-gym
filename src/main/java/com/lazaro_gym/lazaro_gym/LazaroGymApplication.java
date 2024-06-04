ad689d3353d54502f0308818ed38a40bdfe94fb9
arodoo <haro.em@hotmail.com>
2024-06-04 10:44:15 -0600

    firt commit

---
+++ src/main/java/com/lazaro_gym/lazaro_gym/LazaroGymApplication.java
@@ -0,0 +1,13 @@
+package com.lazaro_gym.lazaro_gym;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LazaroGymApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LazaroGymApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lazaro_gym/lazaro_gym/LazaroGymApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lazaro_gym.lazaro_gym;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LazaroGymApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
